491ff8a231527feb7ec4e511d0ef0ff6ae3de858
Svetlozar Georgiev <svetlozar@Svetlozars-MacBook-Pro.local>
2022-09-13 17:41:07 +0300

    obelusstream

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
-import Layout from "../components/Layout";
-import { AuthContextProvider } from "../context/AuthContext";
-import "../styles/globals.css";
+import Layout from '../components/Layout';
+import { AuthContextProvider } from '../context/AuthContext';
+import '../styles/globals.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthContextProvider>
       <Layout>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-D2R5B7T686"
+        ></Script>
+        <Script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-D2R5B7T686');
+        </Script>
         <Component {...pageProps} />
       </Layout>
     </AuthContextProvider>
