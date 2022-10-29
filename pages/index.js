d9e4e4556511065786267fb51dcbfe7d31c50a6c
Svetlozarg <svetlozar.01@abv.bg>
2022-10-29 14:02:07 +0300

    Fixed Issue Index

--- pages/index.js
+++ pages/index.js
@@ -126,29 +126,30 @@ export default function Home({ movies, series }) {
           {/* Iterate over trending movies */}
           {getMoviesInfo.map((movie) => {
             // Get first part of date
-            const splitDate = movie.release_date.split('-');
+            const splitDate = movie?.release_date.split('-');
             return (
               <Link
                 href={{
                   pathname: '/movie',
-                  query: { id: movie.id },
+                  query: { id: movie?.id },
                 }}
-                key={movie.id}
+                key={movie?.id}
                 passHref={true}
               >
                 <a>
                   <MovieCard
-                    id={movie.id}
-                    title={movie.original_title}
-                    description={movie.overview}
+                    id={movie?.id}
+                    title={movie?.original_title}
+                    description={movie?.overview}
                     year={splitDate[0]}
-                    vote={movie.vote_average.toFixed(1)}
+                    vote={movie?.vote_average.toFixed(1)}
                     tag='Movie'
-                    runtime={movie.runtime}
-                    country={movie.production_countries[0].name}
-                    genre={movie.genres}
+                    runtime={movie?.runtime}
+                    country={movie?.production_countries[0]?.name}
+                    genre={movie?.genres}
                     img={
-                      'https://image.tmdb.org/t/p/original/' + movie.poster_path
+                      'https://image.tmdb.org/t/p/original/' +
+                      movie?.poster_path
                     }
                   />
                 </a>
@@ -192,9 +193,9 @@ export default function Home({ movies, series }) {
           {/* Iterate over trending series */}
           {getSeriesInfo.map((serie) => {
             if (
-              serie.vote_average !== 0 &&
-              serie.last_episode_to_air?.season_number !== 0 &&
-              serie.last_episode_to_air?.episode_number !== 0
+              serie?.vote_average !== 0 &&
+              serie?.last_episode_to_air?.season_number !== 0 &&
+              serie?.last_episode_to_air?.episode_number !== 0
             ) {
               // Get first part of date
               const splitDate = serie?.last_air_date?.split('-');
@@ -203,31 +204,31 @@ export default function Home({ movies, series }) {
                 <Link
                   href={{
                     pathname: '/series',
-                    query: { id: serie.id, season: '1', episode: '1' },
+                    query: { id: serie?.id, season: '1', episode: '1' },
                   }}
-                  key={serie.id}
+                  key={serie?.id}
                   passHref={true}
                 >
                   <a>
                     <MovieCard
-                      id={serie.id}
-                      title={serie.name}
+                      id={serie?.id}
+                      title={serie?.name}
                       year={
                         splitDate !== undefined
                           ? splitDate[0]
                           : splitDateFirst[0]
                       }
-                      vote={serie.vote_average.toFixed(1)}
-                      description={serie.overview}
+                      vote={serie?.vote_average.toFixed(1)}
+                      description={serie?.overview}
                       tag='TV'
-                      runtime={serie.episode_run_time[0]}
-                      country={serie.origin_country[0]}
-                      seasons={serie.last_episode_to_air?.season_number}
-                      episodes={serie.last_episode_to_air?.episode_number}
-                      genre={serie.genres}
+                      runtime={serie?.episode_run_time[0]}
+                      country={serie?.origin_country[0]}
+                      seasons={serie?.last_episode_to_air?.season_number}
+                      episodes={serie?.last_episode_to_air?.episode_number}
+                      genre={serie?.genres}
                       img={
                         'https://image.tmdb.org/t/p/original/' +
-                        serie.poster_path
+                        serie?.poster_path
                       }
                     />
                   </a>
